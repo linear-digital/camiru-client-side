26e4c75adf6c5c044cbad105bdc1207c37250cf4
linear-digital <tamizuddin.tr@gmail.com>
2024-05-07 01:02:28 +0600

    update Login.jsx, ResetPassword.jsx, Signup.jsx and Form.jsx

--- src/Pages/Public/Auth/Login.jsx
+++ src/Pages/Public/Auth/Login.jsx
@@ -4,16 +4,17 @@ import Form from './_UI/Form';
 
 const Login = () => {
     return (
-        <div className='p-5 w-full h-full'>
+        <div className='p-5 w-full h-full relative'>
             <div className="flex float-end items-center">
                 <h6 className="text-neutral-800 text-sm mr-1 font-medium">New user?</h6>
                 <div className="text-neutral-800 text-sm font-medium "></div>
                 <Link to={'/signup'} className="text-neutral-800 text-sm font-medium btn-link">Create an acount </Link>
             </div>
-            <div className='flex justify-center items-center h-full'>
+            <div className='flex justify-center items-center w-full h-full'>
                 <Form mode={'login'} />
             </div>
-
+            <div className="absolute right-0 bottom-0 w-full flex justify-center pb-10">
+                <span className="text-zinc-500 text-xs font-normal  leading-none">Protected by reCAPTCHA and subject to the </span><span className="text-cyan-700 text-xs font-normal  leading-none ml-1">Camiru </span><span className="text-teal-600 text-xs font-normal  leading-none ml-1">Privacy Policy</span><span className="text-zinc-500 text-xs font-normal  leading-none mx-1"> and </span><span className="text-teal-600 text-xs font-normal  leading-none">Terms of Service.</span></div>
         </div>
     );
 };
--- src/Pages/Public/Auth/Signup.jsx
+++ src/Pages/Public/Auth/Signup.jsx
@@ -4,16 +4,17 @@ import Form from './_UI/Form';
 
 const Signup = () => {
     return (
-        <div className='p-5 w-full h-full'>
+        <div className='p-5 w-full h-full relative'>
             <div className="flex float-end items-center">
                 <h6 className="text-neutral-800 text-sm mr-1 font-medium">Already have an account?</h6>
                 <div className="text-neutral-800 text-sm font-medium "></div>
                 <Link to={'/login'} className="text-neutral-800 text-sm font-medium btn-link">Sign in</Link>
             </div>
-            <div className='flex justify-center items-center h-full'>
+            <div className='flex justify-center items-center w-full h-full'>
                 <Form mode={'signup'}/>
             </div>
-            
+            <div className="absolute right-0 bottom-0 w-full flex justify-center pb-10">
+                <span className="text-zinc-500 text-xs font-normal  leading-none">Protected by reCAPTCHA and subject to the </span><span className="text-cyan-700 text-xs font-normal  leading-none ml-1">Camiru </span><span className="text-teal-600 text-xs font-normal  leading-none ml-1">Privacy Policy</span><span className="text-zinc-500 text-xs font-normal  leading-none mx-1"> and </span><span className="text-teal-600 text-xs font-normal  leading-none">Terms of Service.</span></div>
         </div>
     );
 };

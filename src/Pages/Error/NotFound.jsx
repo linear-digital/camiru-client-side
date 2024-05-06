f054a75088cfd59a068a557c837fa1a1a26da986
linear-digital <tamizuddin.tr@gmail.com>
2024-05-07 01:05:36 +0600

    update NotFound.jsx

--- src/Pages/Error/NotFound.jsx
+++ src/Pages/Error/NotFound.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Button, Result } from 'antd';
-const NotFound = () => (
-    <Result
-        status="404"
-        title="404"
-        subTitle="Sorry, the page you visited does not exist."
-        extra={<Button role='a' href="/" type="primary">Back Home</Button>}
-        style={{ marginTop: '200px' }}
-    />
-);
+const NotFound = () => {
+    const goback = () => {
+        window.history.back();
+    }
+    return (
+        <Result
+            status="404"
+            title="404"
+            subTitle="Sorry, the page you visited does not exist."
+            extra={<Button onClick={goback} type="primary">Go Back</Button>}
+            style={{ marginTop: '200px' }}
+        />
+    );
+}
 export default NotFound;

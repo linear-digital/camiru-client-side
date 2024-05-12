70391d85297bc27a2d187754311bae6c4e70efe9
linear-digital <tamizuddin.tr@gmail.com>
2024-05-12 22:43:37 +0600

    update Icons.jsx and ScheduleAbsence.jsx

--- src/Pages/Dashboard/User/Profile/_UI/ScheduleAbsence.jsx
+++ src/Pages/Dashboard/User/Profile/_UI/ScheduleAbsence.jsx
@@ -13,141 +13,152 @@ const ScheduleAbsence = () => {
             <div className="flex gap-10">
                 <h1 className="text-slate-900 text-2xl font-bold ">Schedule Absence</h1>
             </div>
-            <div className='mt-10'>
-                <div className="grid grid-cols-5 gap-10 mb-8 items-center">
-                    <div className="col-span-1 justify-end flex items-center">
-                        <h4 className="text-zinc-700 text-sm font-semibold">
-                            Classroom
-                        </h4>
-                    </div>
-                    <div className="col-span-4 flex items-center gap-3">
-                        <div className="w-[300px] h-[31.50px] bg-slate-50 rounded-sm border border-slate-300/opacity-50 grid grid-cols-8" >
-                            <div className='col-span-3 border-r px-1'>
-                                <select
-                                    className='w-full h-full bg-slate-50 border-none outline-none text-xs'
-                                >
-                                    {
-                                        class_rooms.map((item, i) => <option key={i} value={item}>{item}</option>)
-                                    }
-                                </select>
-                            </div>
-                            <div className="col-span-5 flex items-center pr-2">
-                                <input type="text"
-                                    className='w-full h-full bg-slate-50 border-none outline-none text-xs px-2'
-                                    placeholder='Type a note here'
-                                />
-                                <label htmlFor='calendar' className='text-primary cursor-pointer'>
-                                    <FontAwesomeIcon icon={faCalendar} />
-                                </label>
+            <section className='flex items-start'>
+                <div className='mt-10'>
+                    <div className="grid grid-cols-5 gap-10 mb-8 items-center">
+                        <div className="col-span-1 justify-end flex items-center">
+                            <h4 className="text-zinc-700 text-sm font-semibold">
+                                Classroom
+                            </h4>
+                        </div>
+                        <div className="col-span-4 flex items-center gap-3">
+                            <div className="w-[300px] h-[31.50px] bg-slate-50 rounded-sm border border-slate-300/opacity-50 grid grid-cols-8" >
+                                <div className='col-span-3 border-r px-1'>
+                                    <select
+                                        className='w-full h-full bg-slate-50 border-none outline-none text-xs'
+                                    >
+                                        {
+                                            class_rooms.map((item, i) => <option key={i} value={item}>{item}</option>)
+                                        }
+                                    </select>
+                                </div>
+                                <div className="col-span-5 flex items-center pr-2">
+                                    <input type="text"
+                                        className='w-full h-full bg-slate-50 border-none outline-none text-xs px-2'
+                                        placeholder='Type a note here'
+                                    />
+                                    <label htmlFor='calendar' className='text-primary cursor-pointer'>
+                                        <FontAwesomeIcon icon={faCalendar} />
+                                    </label>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="grid grid-cols-5 gap-10 mb-8 items-center">
-                    <div className="col-span-1 justify-end flex items-center">
-                        <h4 className="text-zinc-700 text-sm font-semibold">
-                            Reason
-                        </h4>
-                    </div>
-                    <div className="col-span-4 flex items-center gap-3">
-                        <div className="w-[300px] h-[31.50px] bg-slate-50 rounded-sm border border-slate-300/opacity-50 grid grid-cols-8" >
-                            <div className='col-span-3 border-r px-1'>
-                                <select
-                                    className='w-full h-full bg-slate-50 border-none outline-none text-xs'
-                                >
-                                    <option value="">
-                                        Select Reason
-                                    </option>
-                                    <option value="reason 1">
-                                        Reason 1
-                                    </option>
-                                    <option value="reason 2">
-                                        Reason 2
-                                    </option>
+                    <div className="grid grid-cols-5 gap-10 mb-8 items-center">
+                        <div className="col-span-1 justify-end flex items-center">
+                            <h4 className="text-zinc-700 text-sm font-semibold">
+                                Reason
+                            </h4>
+                        </div>
+                        <div className="col-span-4 flex items-center gap-3">
+                            <div className="w-[300px] h-[31.50px] bg-slate-50 rounded-sm border border-slate-300/opacity-50 grid grid-cols-8" >
+                                <div className='col-span-3 border-r px-1'>
+                                    <select
+                                        className='w-full h-full bg-slate-50 border-none outline-none text-xs'
+                                    >
+                                        <option value="">
+                                            Select Reason
+                                        </option>
+                                        <option value="reason 1">
+                                            Reason 1
+                                        </option>
+                                        <option value="reason 2">
+                                            Reason 2
+                                        </option>
 
-                                    <option value="reason 3">
-                                        Reason 3
-                                    </option>
-                                </select>
-                            </div>
-                            <div className="col-span-5 flex items-center pr-2">
-                                <input type="text"
-                                    className='w-full h-full bg-slate-50 border-none outline-none text-xs px-2'
-                                    placeholder='Type a note here'
-                                />
-                                <label htmlFor='calendar' className='text-primary cursor-pointer'>
-                                    <FontAwesomeIcon icon={faCalendar} />
-                                </label>
+                                        <option value="reason 3">
+                                            Reason 3
+                                        </option>
+                                    </select>
+                                </div>
+                                <div className="col-span-5 flex items-center pr-2">
+                                    <input type="text"
+                                        className='w-full h-full bg-slate-50 border-none outline-none text-xs px-2'
+                                        placeholder='Type a note here'
+                                    />
+                                    <label htmlFor='calendar' className='text-primary cursor-pointer'>
+                                        <FontAwesomeIcon icon={faCalendar} />
+                                    </label>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    {/* Start Date  */}
+                    <div className="grid grid-cols-5 gap-10 mb-8 items-center">
+                        <div className="col-span-1 justify-end flex items-center">
+                            <h4 className="text-zinc-700 text-sm font-semibold">
+                                Start Date
+                            </h4>
+                        </div>
+                        <div className="col-span-4 flex items-center gap-3">
+                            <label htmlFor='calendar' className="w-[300px] h-[31.50px] bg-slate-50 rounded-sm border border-slate-300/opacity-50 text-xs text-gray-600 flex items-center justify-start px-4" >
+                                <input type="date" className='w-full h-full bg-slate-50 border-none outline-none ' id='calendar'
+
+                                />
+                            </label>
+
+                        </div>
+                    </div>
+                    {/* End Date  */}
+                    <div className="grid grid-cols-5 gap-10 mb-8 items-center">
+                        <div className="col-span-1 justify-end flex items-center">
+                            <h4 className="text-zinc-700 text-sm font-semibold">
+                                End Date
+                            </h4>
+                        </div>
+                        <div className="col-span-4 flex items-center gap-3">
+                            <label htmlFor='calendar' className="w-[300px] h-[31.50px] bg-slate-50 rounded-sm border border-slate-300/opacity-50 text-xs text-gray-600 flex items-center justify-start px-4" >
+                                <input type="date" className='w-full h-full bg-slate-50 border-none outline-none ' id='calendar'
+
+                                />
+                            </label>
+
+                        </div>
+                    </div>
+                    {/* Note   */}
+                    <div className="grid grid-cols-5 gap-10 mb-8 items-start">
+                        <div className="col-span-1 justify-end flex items-center">
+                            <h4 className="text-zinc-700 text-sm font-semibold">
+                                Note
+                            </h4>
+                        </div>
+                        <div className="col-span-4 flex items-center gap-3">
+                            <textarea name="" id="" className='w-[300px] border outline-none text-xs p-3 h-[131px]'
+                                placeholder='Type note here'
+                            >
+
+                            </textarea>
+
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-5 gap-10 mt-5 items-start">
+                        <div className="col-span-1 justify-end flex items-center">
+
+                        </div>
+                        <div className="col-span-4">
+                            <button
+                                className=' bg-[#c6f2eccd] text-xs px-5  w-[135px] font-semibold text-[#24a899] h-[30px] rounded-md'
+                            >
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+
                 </div>
-                {/* Start Date  */}
-                <div className="grid grid-cols-5 gap-10 mb-8 items-center">
-                    <div className="col-span-1 justify-end flex items-center">
-                        <h4 className="text-zinc-700 text-sm font-semibold">
-                            Start Date
-                        </h4>
-                    </div>
-                    <div className="col-span-4 flex items-center gap-3">
-                        <label htmlFor='calendar' className="w-[300px] h-[31.50px] bg-slate-50 rounded-sm border border-slate-300/opacity-50 text-xs text-gray-600 flex items-center justify-start px-4" >
-                            <input type="date" className='w-full h-full bg-slate-50 border-none outline-none ' id='calendar'
-
-                            />
-                        </label>
-
-                    </div>
-                </div>
-                {/* End Date  */}
-                <div className="grid grid-cols-5 gap-10 mb-8 items-center">
-                    <div className="col-span-1 justify-end flex items-center">
-                        <h4 className="text-zinc-700 text-sm font-semibold">
-                            End Date
-                        </h4>
-                    </div>
-                    <div className="col-span-4 flex items-center gap-3">
-                        <label htmlFor='calendar' className="w-[300px] h-[31.50px] bg-slate-50 rounded-sm border border-slate-300/opacity-50 text-xs text-gray-600 flex items-center justify-start px-4" >
-                            <input type="date" className='w-full h-full bg-slate-50 border-none outline-none ' id='calendar'
-
-                            />
-                        </label>
-
-                    </div>
-                </div>
-                {/* Note   */}
-                <div className="grid grid-cols-5 gap-10 mb-8 items-start">
-                    <div className="col-span-1 justify-end flex items-center">
-                        <h4 className="text-zinc-700 text-sm font-semibold">
-                            Note
-                        </h4>
-                    </div>
-                    <div className="col-span-4 flex items-center gap-3">
-                        <textarea name="" id="" className='w-[300px] border outline-none text-xs p-3 h-[131px]'
-                            placeholder='Type note here'
-                        >
-
-                        </textarea>
-
-                    </div>
-                </div>
-                <div className="grid grid-cols-5 gap-10 mt-5 items-start">
-                    <div className="col-span-1 justify-end flex items-center">
-
-                    </div>
-                    <div className="col-span-4">
-                        <button
-                            className=' bg-[#c6f2eccd] text-xs px-5  w-[135px] font-semibold text-[#24a899] h-[30px] rounded-md'
-                        >
-                            Submit
-                        </button>
-                    </div>
-                </div>
-
-            </div>
-        </div >
-    );
-};
-
+                <div className="w-[264px] h-[136.72px] bg-teal-300/20 rounded-[9.16px] flex justify-center items-center px-5" >
+                    <p className="">
+                        <span className="text-teal-600 text-[9.51px] font-normal ">Set planned absences in advance for a child or teacher.  
+                        </span>
+                    <br />
+                    <span className="text-teal-600 text-[9.51px] font-semibold ">Note:</span>
+<span className="text-teal-600 text-[9.51px] font-normal "> The person will only be marked absent for the days he/she is scheduled to be present in the designated period.</span>
+                </p>
+             </div>
+        </section>      
+        </div>               
+    );      
+};      
+      
 export default ScheduleAbsence;
 
 const Row = ({ title, desc }) => {

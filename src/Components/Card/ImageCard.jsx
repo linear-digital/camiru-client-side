c4feec3729303bb3abf5f0905dc359ed77601f74
linear-digital <tamizuddin.tr@gmail.com>
2024-05-14 21:49:27 +0600

    update ImageCard.jsx, Photos_Videos.jsx and icons.jsx

--- src/Components/Card/ImageCard.jsx
+++ src/Components/Card/ImageCard.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 const ImageCard = () => {
     return (
-        <div className="w-full h-[160.85px] bg-sky-100 rounded-[4.85px] border bg-cover bg-center cursor-pointer"
+        <div className="w-full h-[160.85px] bg-sky-100 rounded-[6px] border bg-cover bg-center cursor-pointer overflow-hidden poppins"
             style={{
-                backgroundImage: 'url("https://classplusapp.com/growth/wp-content/uploads/2022/06/8-Tips-to-Upgrade-to-Design-Classroom.jpg")',
+                backgroundImage: 'url("https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?cs=srgb&dl=pexels-pixabay-206359.jpg&fm=jpg")',
             }}
         >
-            <div className="w-full h-full flex flex-col justify-end items-start px-5 pb-3 bg-black/20">
+            <div className="w-full h-full flex flex-col justify-end items-start px-5 pb-3 bg-black/30">
                 <h1 className="text-white text-sm font-semibold">Classname</h1>
-                <div className="text-white text-xs font-light font-['Poppins']">16:29, 12th Decamber, 2020</div>
+                <div className="text-white text-xs font-light ">16:29, 12th Decamber, 2020</div>
             </div>
         </div>
     );
--- src/Pages/Dashboard/PhotosVideos/Photos_Videos.jsx
+++ src/Pages/Dashboard/PhotosVideos/Photos_Videos.jsx
@@ -3,23 +3,62 @@ import { Card } from '@material-tailwind/react';
 import React from 'react';
 import ImageCard from '../../../Components/Card/ImageCard';
 import { faCircleDown } from '@fortawesome/free-regular-svg-icons';
+import { ClassRoomSelector } from '../../../Components/Card/Dropdown';
+import { class_rooms } from '../../../util/classrooms';
+import { Bars, Grids } from '../../../util/icons';
+
 
 const Photos_Videos = () => {
+
     return (
-        <div className='p-5 w-full mt-10 shadow-blue-gray-50'>
+        <div className='p-10 bg-white rounded-lg'>
             <div className="flex justify-between items-center">
                 <div>
                     <h3 className="text-zinc-800 text-2xl font-semibold ">Recent Photos & videos</h3>
                     <p className=" text-neutral-400 text-xs mt-1 font-normal">Uploading photos & Videos is the #1 way to engage parents.<br /></p>
                 </div>
-                <div>
-                    <button className="w-[150px] h-[22px] bg-orange-400 rounded-sm text-xs flex items-center gap-1 px-2" >
+                <div className='flex gap-5 items-center'>
+                    <ClassRoomSelector options={class_rooms}
+                        color={"#FFBB3B"} border={"#FFBB3B"}
+                    />
+                    <ClassRoomSelector options={[
+                        "Photos", "Videos"
+                    ]}
+                        color={"#FFBB3B"} border={"#FFBB3B"}
+                    />
+                    <button className='w-9 h-9 flex items-center justify-center rounded-md bg-[#FFBB3B33] border-none '>
+                        <Bars />
+                    </button>
+                    <button className='w-9 h-9 flex items-center justify-center rounded-md bg-[#FFBB3B33] border-none '>
+                        <Grids />
+                    </button>
+                    <button className="w-[150px] h-[36px] bg-orange-400 text-xs flex items-center gap-1 px-2 rounded" >
                         <span className=" text-zinc-800 text-xs font-medium ">12th January, 2022</span>
                         <FontAwesomeIcon icon={faCircleDown} />
                     </button>
                 </div>
             </div>
-            <div className="grid grid-cols-3 mt-10 gap-5">
+            <div className="grid grid-cols-4 mt-10 gap-5">
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
+                <ImageCard />
                 <ImageCard />
                 <ImageCard />
                 <ImageCard />

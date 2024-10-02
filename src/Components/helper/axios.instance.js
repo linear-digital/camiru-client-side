3ffdf44d294b3f7787b6bd592d7921af3d49bf55
linear-digital <tamizuddin.tr@gmail.com>
2024-10-02 21:03:27 +0600

    update axios.instance.js

--- src/Components/helper/axios.instance.js
+++ src/Components/helper/axios.instance.js
@@ -6,7 +6,7 @@ const local = "http://localhost:4000/api"
 const server = 'https://server.camiru.com/api'
 const url = local
 export const api = axios.create({
-    baseURL: url,
+    baseURL: server,
     headers: {
         "Content-Type": "application/json",
         "token": Cookie.get('token-camiru')

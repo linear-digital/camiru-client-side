a6c8482cd6ac5245e3ffccec2ae5b22d9c74d186
linear-digital <tamizuddin.tr@gmail.com>
2024-10-11 00:37:11 +0600

    update App.css, AbsRow.jsx, AbsentStudentList.jsx and src/index.css

--- src/Pages/Dashboard/_UI/AbsRow.jsx
+++ src/Pages/Dashboard/_UI/AbsRow.jsx
@@ -1,21 +1,24 @@
 import { Avatar } from 'antd';
 
 import React from 'react';
+import nameDisplay from '../../../util/nameDisplay';
+import { imageUrl } from '../../../Components/helper/axios.instance';
 
-const AbsRow = () => {
+const AbsRow = ({ item }) => {
     return (
         <div className='flex items-center justify-between'>
 
             <div className="flex items-center gap-3">
                 <div className="avatar  border rounded-full overflow-hidden">
                     <div className="w-7 h-7 rounded-full">
-                        <Avatar 
+                        <Avatar
                             className='w-full h-full object-cover'
                             alt='attachment'
+                            src={imageUrl(item?.profilePic)}
                         />
                     </div>
                 </div>
-                <div className=" text-black text-xs font-normal leading-relaxed tracking-tight">Md Jibon Hossain Rifat</div>
+                <div className=" text-black text-xs font-normal leading-relaxed tracking-tight">{nameDisplay(item)}</div>
             </div>
             <button className='btn gap-2 btn-secondary bg-[#96C82C] rounded-3xl btn-xs py-2  px-6 text-[10px] text-white'>
                 View
--- src/Pages/Dashboard/_UI/AbsentStudentList.jsx
+++ src/Pages/Dashboard/_UI/AbsentStudentList.jsx
@@ -6,16 +6,34 @@ import AbsRow from './AbsRow';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { Dropdown } from 'antd';
 import { useSelector } from 'react-redux';
+import { useQuery } from '@tanstack/react-query';
+import { fetcher } from '../../../Components/helper/axios.instance';
+import Loader from '../../../Components/Loader';
 
 const AbsentStudentList = () => {
     const { classrooms } = useSelector(state => state.classroom)
+    const { currentUser } = useSelector(state => state.user)
+    const [search, setSearch] = React.useState('')
+    const { data, isLoading } = useQuery({
+        queryKey: ['absent-student', classrooms[0]?._id, search],
+        queryFn: async () => {
+            const res = await fetcher({
+                url: `/student/center/${currentUser?._id}?classroom=${search || classrooms[0]?._id}`,
+                method: 'GET',
+            })
+            return res
+        }
+    })
+    if (isLoading) {
+        return <Loader />
+    }
     return (
-        <Card className='p-5 w-full h-full border border-[#C7F1FF] bg-[#F8FCFF] shadow-none'>
+        <Card className='p-5 w-full h-full max-h-[350px] border border-[#C7F1FF] bg-[#F8FCFF] shadow-none'>
             <section className="flex items-center justify-between">
                 <h2 className=" text-zinc-800 text-xs font-semibold ">Absent Student List</h2>
                 <div className="flex">
-                    <Dropdown menu={{ items: classrooms.map(item => ({ key: item._id, label: item.name })) }} trigger={['click']}>
-                        <button className='flex gap-2 btn-secondary bg-[#96C82C] rounded-3xl btn-xs py-2 px-4 text-[10px] text-white'>
+                    <Dropdown menu={{ items: classrooms.map(item => ({ key: item._id, label: <button onClick={() => setSearch(item?._id)}>{item.name}</button> })) }} trigger={['click']}>
+                        <button className='flex items-center gap-2 btn-secondary bg-[#96C82C] rounded-3xl btn-xs py-2 px-4 text-[10px] text-white'>
                             <span>  {classrooms[0]?.name}</span>
                             <FontAwesomeIcon icon={faChevronDown} />
                         </button>
@@ -26,12 +44,12 @@ const AbsentStudentList = () => {
                     </button>
                 </div>
             </section>
-            <section className='flex flex-col gap-3 mt-5'>
-                <AbsRow />
-                <AbsRow />
-                <AbsRow />
-                <AbsRow />
-                <AbsRow />
+            <section className='flex flex-col gap-3 mt-5 h-full overflow-y-auto'>
+                {
+                    data?.map((item, index) => (
+                        <AbsRow key={index} item={item} />
+                    ))
+                }
             </section>
         </Card>
     );
